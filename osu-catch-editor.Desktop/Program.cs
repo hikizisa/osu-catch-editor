bf7f9c0b228bbeea4f0e882ad5648bf35dae72ec
hikizisa <32244151+hikizisa@users.noreply.github.com>
2020-08-02 23:15:10 +0900

    initial lize with osu!framework template

---
+++ osu-catch-editor.Desktop/Program.cs
@@ -0,0 +1,16 @@
+﻿using osu.Framework.Platform;
+using osu.Framework;
+using osu_catch_editor.Game;
+
+namespace osu_catch_editor.Desktop
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            using (GameHost host = Host.GetSuitableHost(@"osu_catch_editor"))
+            using (osu.Framework.Game game = new osu_catch_editorGame())
+                host.Run(game);
+        }
+    }
+}
---
+++ osu-catch-editor.Resources/osu-catch-editorResources.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace osu_catch_editor.Resources
+{
+    public static class osu_catch_editorResources
+    {
+        public static Assembly ResourceAssembly => typeof(osu_catch_editorResources).Assembly;
+    }
+}
