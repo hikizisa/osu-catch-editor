c224017c26861af26ac2fbc521fbcceec6854e7d
hikizisa <32244151+hikizisa@users.noreply.github.com>
2020-08-03 13:24:47 +0900

    renamed again

---
+++ osuCatchEditor.Desktop/Program.cs
@@ -0,0 +1,16 @@
+﻿using osu.Framework.Platform;
+using osu.Framework;
+using osuCatchEditor.Game;
+
+namespace osuCatchEditor.Desktop
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            using (GameHost host = Host.GetSuitableHost(@"osuCatchEditor"))
+            using (osu.Framework.Game game = new osuCatchEditorGame())
+                host.Run(game);
+        }
+    }
+}
---
+++ osuCatchEditor.Resources/osuCatchEditorResources.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace osuCatchEditor.Resources
+{
+    public static class osuCatchEditorResources
+    {
+        public static Assembly ResourceAssembly => typeof(osuCatchEditorResources).Assembly;
+    }
+}
